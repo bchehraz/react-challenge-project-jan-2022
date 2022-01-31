94b90fc4ecc44dd483e4fd110b6ac91ea2abc3d7
Babak Chehraz <babak.chehraz@gmail.com>
2022-01-30 17:23:25 -0800

    test(ordersList component): adds tests to ensure each order correctly displays createdAt in hh:mm:ss format

--- application/src/components/view-orders/ordersList.test.js
+++ application/src/components/view-orders/ordersList.test.js
@@ -55,4 +55,38 @@ describe('Orders List', () => {
         expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
 
     });
+
+    test('renders a single order with a midnight timestamp in proper hh:mm:ss format', () => {
+      const orders = [
+        {
+            order_item: "Pasta",
+            quantity: "1",
+            _id: 1,
+            createdAt: "2022-01-30T08:00:00.973Z"
+        },
+      ];
+      render(
+        <OrdersList
+            orders={orders}
+        />
+      )
+      expect(screen.getByText('Order placed at 24:00:00')).toBeInTheDocument();
+    })
+
+    test('renders a single order with 1:05 AM timestamp in proper hh:mm:ss format', () => {
+      const orders = [
+        {
+            order_item: "Food",
+            quantity: "35",
+            _id: 1,
+            createdAt: "2022-01-30T09:05:09.973Z"
+        },
+      ];
+      render(
+        <OrdersList
+            orders={orders}
+        />
+      )
+      expect(screen.getByText('Order placed at 01:05:09')).toBeInTheDocument();
+    });
 })
