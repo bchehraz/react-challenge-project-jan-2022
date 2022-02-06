af485087738e9422230ca35d2328284666622d2e
Babak Chehraz <babak.chehraz@gmail.com>
2022-02-05 19:17:58 -0800

    feat(ordersList): implement delete order feature

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Template } from '../../components';
-import { SERVER_IP } from '../../private';
-import OrdersList from './ordersList';
-import './viewOrders.css';
+import React, { useState, useEffect } from "react";
+import { Template } from "../../components";
+import { SERVER_IP } from "../../private";
+import OrdersList from "./ordersList";
+import "./viewOrders.css";
 
 export default function ViewOrders(props) {
-    const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        fetch(`${SERVER_IP}/api/current-orders`)
-            .then(response => response.json())
-            .then(response => {
-                if(response.success) {
-                    setOrders(response.orders);
-                } else {
-                    console.log('Error getting orders');
-                }
-            });
-    }, [])
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
-    return (
-        <Template>
-            <div className="container-fluid">
-                <OrdersList
-                    orders={orders}
-                />
-            </div>
-        </Template>
-    );
+  function fetchOrders() {
+    fetch(`${SERVER_IP}/api/current-orders`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.success) {
+          setOrders(response.orders);
+          return response.orders;
+        } else {
+          console.log("Error getting orders");
+        }
+      });
+  }
+
+  return (
+    <Template>
+      <div className="container-fluid">
+        <OrdersList orders={orders} fetchOrders={fetchOrders} />
+      </div>
+    </Template>
+  );
 }
