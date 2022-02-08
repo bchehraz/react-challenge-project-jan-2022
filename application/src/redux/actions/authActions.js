7cc4b1b40fe22c2da6f624259bb1531608449eea
Babak Chehraz <babak.chehraz@gmail.com>
2022-02-07 21:25:05 -0800

    feat(authActions): add registerUser redux action

--- application/src/redux/actions/authActions.js
+++ application/src/redux/actions/authActions.js
@@ -1,39 +1,80 @@
-import { LOGIN, LOGOUT } from './types';
-import { SERVER_IP } from '../../private'
+import { LOGIN, LOGOUT } from "./types";
+import { SERVER_IP } from "../../private";
 
 const finishLogin = (email, token) => {
-    return {
-        type: LOGIN,
-        payload: {
-            email,
-            token,
+  return {
+    type: LOGIN,
+    payload: {
+      email,
+      token,
+    },
+  };
+};
+
+const loginUser = (email, password) => {
+  return async (dispatch) => {
+    return fetch(`${SERVER_IP}/api/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.success) {
+          dispatch(finishLogin(response.email, response.token));
+          return { success: true };
         }
-    }
-}
 
-export const loginUser = (email, password) => {
-    return (dispatch) => {
-        fetch(`${SERVER_IP}/api/login`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(response => response.json())
-        .then(response => {
-            if (response.success) {
-                dispatch(finishLogin(response.email, response.token));
-            }
-        })
-    };
-}
+        return {
+          success: false,
+          error: response.error,
+        };
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+};
 
-export const logoutUser = () => {
-    return {
-        type: LOGOUT,
-        payload: null,
-    }
-}
+const registerUser = (email, password, confirmPassword) => {
+  return async (dispatch) => {
+    return fetch(`${SERVER_IP}/api/register`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        confirmPassword,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.success) {
+          return {
+            success: false,
+            error: response.error,
+          };
+        }
+        return { success: true };
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT,
+    payload: null,
+  };
+};
+
+export { logoutUser, loginUser, registerUser };
