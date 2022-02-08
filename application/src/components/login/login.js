8b2430fc075a383f6207f7639cd17820b5ae1132
Babak Chehraz <babak.chehraz@gmail.com>
2022-02-07 21:30:19 -0800

    feat(login): add link to register page from login

--- application/src/components/login/login.js
+++ application/src/components/login/login.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import LoginForm from './login-form/loginForm';
-import './login.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import LoginForm from "./login-form/loginForm";
+import "./login.css";
 
 class Login extends Component {
-  
   render() {
     return (
       <div className="main-body">
         <h1 className="text-center">Login Screen</h1>
         <div className="d-flex justify-content-center mt-5">
-          <LoginForm onLogin={() => {this.props.history.push('/view-orders')}}/>
+          <LoginForm
+            onLogin={() => {
+              this.props.history.push("/view-orders");
+            }}
+          />
         </div>
+        <br />
+        Don't have an account? <Link to="/register"> Register here </Link>
       </div>
-    )
+    );
   }
 }
 
